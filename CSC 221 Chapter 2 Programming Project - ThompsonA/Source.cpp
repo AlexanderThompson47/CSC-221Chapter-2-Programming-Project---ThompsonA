4c016f3c1a44677d403fec1bea6d12a0fde1cd4b
AlexanderThompson47 <thompsonalexander287@gmail.com>
2025-02-20 02:51:51 -0500

    commit all push

--- CSC 221 Chapter 2 Programming Project - ThompsonA/Source.cpp
+++ CSC 221 Chapter 2 Programming Project - ThompsonA/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     const double TANK_CAPACITY = 20.0;      // gallons
     const double MPG_TOWN = 23.5;           // miles per gallon in town
-    const double MPG_HIGHWAY = 28.9;        // miles per gallon on the highway
+    const double MPG_HIGHWAY = 28.9;        // miles per gallon on the highway 
 
     double distance_in_town = TANK_CAPACITY * MPG_TOWN;
     double distance_on_highway = TANK_CAPACITY * MPG_HIGHWAY;
